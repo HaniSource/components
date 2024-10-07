46c121b239bea896faf71f2a6c7112db823d4d0c
CharrafiMed <mhch5003@gmail.com>
2024-10-07 21:44:31 +0200

    wip

--- components/tabs-category/special-tabs/files/example.blade.php
+++ components/tabs-category/special-tabs/files/example.blade.php
@@ -18,13 +18,27 @@
             <div class="mx-auto max-w-3xl px-2">
                 <div
                     class="prose max-w-none dark:prose-invert prose-pre:bg-white/5 dark:prose-code:rounded dark:prose-code:bg-white/20">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dicta fuga quo omnis modi illo, delectus eos natus, sint cum porro commodi? Iste perspiciatis molestiae voluptate obcaecati perferendis. Mollitia, unde. Ex illum autem quo commodi natus amet ad ut error, eum fuga eos, quod modi, placeat sequi unde libero corporis suscipit cumque quidem delectus veritatis dolores saepe sed. Enim eum iure obcaecati, eos ullam tenetur. Neque ab fugiat, molestias autem ullam fugit, architecto dicta iusto eum consequuntur voluptas deleniti laborum voluptatem ipsum, quibusdam iure eligendi rem itaque reiciendis! Cupiditate quasi minima beatae, sequi consectetur sit dolor nemo voluptatem. Aperiam, labore.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem culpa aspernatur vel fugit
+                    magni tenetur sapiente reiciendis eveniet. Deleniti, esse! Rem a nihil sequi numquam quaerat culpa
+                    repudiandae molestiae mollitia nemo accusamus, veritatis ex, magnam architecto inventore harum vel
+                    voluptas illum autem qui cumque sed animi totam aspernatur! Provident nesciunt recusandae,
+                    consequatur perspiciatis unde culpa maxime illum numquam at rem vero adipisci in saepe tempore fuga
+                    aliquid reiciendis nisi vel pariatur nam corrupti nihil, molestiae nemo. Aliquid, quidem! Ipsa
+                    ducimus officia quia cum placeat quae accusantium nobis commodi repudiandae sed. Perferendis minus
+                    repellendus qui iure velit sequi nulla quidem nostrum!
                 </div>
             </div>
-            
+
         </x-tabs.panel>
         <x-tabs.panel>
-            Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. Ut vel doloribus repellat nemo cumque et rerum omnis, autem culpa repellendus, illo consequuntur nostrum? Dolore eaque obcaecati maiores eius repudiandae nobis, nam perferendis laboriosam, officia amet quo, doloremque ab reprehenderit! Quia error, fugit sunt, sapiente, qui libero minima harum adipisci laudantium ad blanditiis quaerat animi beatae consectetur. Voluptatem iste esse cumque, aperiam quos repellat harum. Vel veniam id blanditiis animi exercitationem quia delectus ducimus esse illo laborum, reprehenderit quos quo eius repudiandae illum aperiam corporis? Mollitia, rerum reiciendis optio molestias dolorum aut autem quae voluptatibus. Fuga architecto atque molestiae quia velit?
+            Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. Ut vel doloribus repellat nemo cumque et rerum
+            omnis, autem culpa repellendus, illo consequuntur nostrum? Dolore eaque obcaecati maiores eius repudiandae
+            nobis, nam perferendis laboriosam, officia amet quo, doloremque ab reprehenderit! Quia error, fugit sunt,
+            sapiente, qui libero minima harum adipisci laudantium ad blanditiis quaerat animi beatae consectetur.
+            Voluptatem iste esse cumque, aperiam quos repellat harum. Vel veniam id blanditiis animi exercitationem quia
+            delectus ducimus esse illo laborum, reprehenderit quos quo eius repudiandae illum aperiam corporis?
+            Mollitia, rerum reiciendis optio molestias dolorum aut autem quae voluptatibus. Fuga architecto atque
+            molestiae quia velit?
         </x-tabs.panel>
     </x-slot:panels>
 </x-tabs>
