bbf177c092e39340ef498b20d320321e71c927cf
CharrafiMed <mhch5003@gmail.com>
2024-11-01 11:17:59 +0100

    wip

---
+++ components/textarea-category/auto-size-texterea/files/usage.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <x-components::inputs.text-area 
+        class="w-full"
+        placeholder="write a comment"
+        rows="4" 
+        name="textarea"
+        wire:model="comment.body" 
+    />
+</div>
