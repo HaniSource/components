d00e844fa90e8b83663294ecb959017d3f2aaaf4
charrafimed <mhch5003@gmail.com>
2025-08-01 16:39:00 +0200

    delete old resources

--- components/modals-category/modal/files/usage.blade.php
+++
@@ -1,9 +0,0 @@
-<x-modal>
-    <x-slot:trigger class="px-4 py-1 rounded-xl dark:text-gray-300 text-gray-800 bg-white dark:bg-white/5">
-            open this modal
-    </x-slot:trigger>
-    <x-slot:header>
-            Hey, you know what is modal.
-    </x-slot:header>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus pariatur autem dolor doloremque, tempore omnis provident? Quasi, minima.
-</x-modal>
