fd07018c93ad4eb658971a47f905dff0ffeb3ae8
charrafimed <mhch5003@gmail.com>
2025-08-17 20:52:35 +0200

    textarea is ready

--- components/textarea/files/index.blade.php
+++ components/textarea/files/index.blade.php
@@ -4,6 +4,7 @@
     'resize' => 'vertical',
     'name' => $attributes->whereStartsWith('wire:model')->first(),
     'rows' => null,
+    'persist' => false,
     'invalid' => null,
     ])
 @php
@@ -11,21 +12,37 @@
 
     $initialHeight = (($rows) * 1.5) + 0.75;
 
+    $persist = filled($persistenceKey = $persist && $name ? $name . '-value' : null);
+
     $classes = [
-        'block p-2 w-full text-base sm:text-sm text-gray-800 disabled:text-gray-500 placeholder-gray-400 disabled:placeholder-gray-400/70 dark:text-gray-300 dark:disabled:text-gray-400 dark:placeholder-gray-400 dark:disabled:placeholder-gray-500', // texts colors
-        'bg-white dark:bg-white/10 dark:disabled:bg-white/[0.06]', // background
-        'disabled:cursor-not-allowed', // cursor handling
-        'transition-colors duration-200', // transition handling 
-        'shadow-sm disabled:shadow-none border rounded-lg', // shadows
-        'focus:ring-2 focus:ring-offset-0 focus:outline-none', // focus state
-        'border-gray-300 focus:border-gray-500 focus:ring-gray-500/20 dark:border-gray-600 dark:focus:border-gray-400 dark:focus:ring-gray-400/20'=> !$invalid, //  focus state handling  
-        'border-red-500 focus:border-red-500 focus:ring-red-500/20 dark:border-red-400 dark:focus:border-red-400 dark:focus:ring-red-400/20' => $invalid, // invalid focus state handling 
+        // Text colors
+        'inline-block p-2 w-full text-base sm:text-sm text-neutral-800 disabled:text-neutral-500 placeholder-neutral-400 disabled:placeholder-neutral-400/70 dark:text-neutral-300 dark:disabled:text-neutral-400 dark:placeholder-neutral-400 dark:disabled:placeholder-neutral-500',
+
+        // Background
+        'bg-white dark:bg-neutral-900 dark:disabled:bg-neutral-800',
+
+        // Cursor and transitions
+        'disabled:cursor-not-allowed transition-colors duration-200',
+
+        // Shadows and borders
+        'shadow-sm disabled:shadow-none border rounded-box',
+
+        // Focus outline
+        'focus:ring-2 focus:ring-offset-0 focus:outline-none',
+
+        // Normal state borders and focus rings
+        'border-black/10 focus:border-black/15 focus:ring-neutral-900/15 dark:border-white/10 dark:focus:border-white/20 dark:focus:ring-neutral-100/15' => !$invalid,
+
+        // Invalid state borders and focus rings
+        'border-red-500 focus:border-red-500 focus:ring-red-500/25 dark:border-red-400 dark:focus:border-red-400 dark:focus:ring-red-400/25' => $invalid,
+
+        // Resize handling
         match ($resize) {
             'none' => 'resize-none',
             'both' => 'resize',
             'horizontal' => 'resize-x',
             'vertical' => 'resize-y',
-        }, 
+        },
     ];
 @endphp
 
@@ -47,10 +64,10 @@
                 this.$el.style.height = newHeight;
             }
         }
-    }"  
+    }"
     x-init="
         if(!this.$el) return;
-        // give our textarea initial height based on the provided rows 
+        // give our textarea initial height based on the provided rows
         this.$el.style.height = this.initialHeight;
 
         const observer = new ResizeObserver(() => {
@@ -71,4 +88,3 @@
     x-model="contents"
     x-modelable="contents"
 ></textarea>
-
